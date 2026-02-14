d9435f93f33b4bcd2c7f453720de64fe9d9252b3
parthmern <parthmern@gmail.com>
2026-02-13 19:22:17 -0500

    lab5 add

--- lab5/my-app/middleware.js
+++ lab5/my-app/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export function middleware(req) {
   if (req.nextUrl.pathname.startsWith("/api")) {
-    console.log(`API ${req.method} ${req.nextUrl.pathname}`);
+    console.log(`API ==> ${req.method} ${req.nextUrl.pathname}`);
   }
 
   return NextResponse.next(); // calling next original handler
