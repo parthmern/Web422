1c84a631a1fb0b4089735101ac46e2d8212daf7a
parthmern <parthmern@gmail.com>
2026-04-06 18:21:14 -0400

    userservice update

--- as1/user-api/server.js
+++ as1/user-api/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -10,57 +10,73 @@ const HTTP_PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.json({ message: "parth patel 128823234 web422 user-service working" });
+});
+
 app.post("/api/user/register", (req, res) => {
-    userService.registerUser(req.body)
+  userService
+    .registerUser(req.body)
     .then((msg) => {
-        res.json({ "message": msg });
-    }).catch((msg) => {
-        res.status(422).json({ "message": msg });
+      res.json({ message: msg });
+    })
+    .catch((msg) => {
+      res.status(422).json({ message: msg });
     });
 });
 
 app.post("/api/user/login", (req, res) => {
-    userService.checkUser(req.body)
+  userService
+    .checkUser(req.body)
     .then((user) => {
-        res.json({ "message": "login successful"});
-    }).catch(msg => {
-        res.status(422).json({ "message": msg });
+      res.json({ message: "login successful" });
+    })
+    .catch((msg) => {
+      res.status(422).json({ message: msg });
     });
 });
 
 app.get("/api/user/favourites", (req, res) => {
-    userService.getFavourites(req.user._id)
-    .then(data => {
-        res.json(data);
-    }).catch(msg => {
-        res.status(422).json({ error: msg });
+  userService
+    .getFavourites(req.user._id)
+    .then((data) => {
+      res.json(data);
     })
-
+    .catch((msg) => {
+      res.status(422).json({ error: msg });
+    });
 });
 
 app.put("/api/user/favourites/:id", (req, res) => {
-    userService.addFavourite(req.user._id, req.params.id)
-    .then(data => {
-        res.json(data)
-    }).catch(msg => {
-        res.status(422).json({ error: msg });
+  userService
+    .addFavourite(req.user._id, req.params.id)
+    .then((data) => {
+      res.json(data);
     })
+    .catch((msg) => {
+      res.status(422).json({ error: msg });
+    });
 });
 
 app.delete("/api/user/favourites/:id", (req, res) => {
-    userService.removeFavourite(req.user._id, req.params.id)
-    .then(data => {
-        res.json(data)
-    }).catch(msg => {
-        res.status(422).json({ error: msg });
+  userService
+    .removeFavourite(req.user._id, req.params.id)
+    .then((data) => {
+      res.json(data);
     })
+    .catch((msg) => {
+      res.status(422).json({ error: msg });
+    });
 });
 
-userService.connect()
-.then(() => {
-    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
-})
-.catch((err) => {
+userService
+  .connect()
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log("API listening on: " + HTTP_PORT);
+    });
+  })
+  .catch((err) => {
     console.log("unable to start the server: " + err);
     process.exit();
-});
+  });
